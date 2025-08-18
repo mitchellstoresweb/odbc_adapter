f58758b7d2c9a0ef6e5d2542f216d4d8f4efe3bd
erick-mitchells <erick@mitchells.com>
2025-08-18 14:11:40 -0400

    replace initializer with a Railtie

--- config/initializers/register_odbc_adapter.rb
+++
@@ -1,5 +0,0 @@
-ActiveRecord::ConnectionAdapters.register(
-  "odbc", 
-  "ActiveRecord::ConnectionAdapters::ODBCAdapter", 
-  "active_record/connection_adapters/odbc_adapter.rb"
-)
--- lib/odbc_adapter.rb
+++ lib/odbc_adapter.rb
@@ -1,2 +1,13 @@
 # Requiring with this pattern to mirror ActiveRecord
 require 'active_record/connection_adapters/odbc_adapter'
+
+# Register the ODBC adapter with ActiveRecord
+class Railtie < Rails::Railtie
+  initializer "odbc_adapter.configure" do |app|
+    ActiveRecord::ConnectionAdapters.register(
+      "odbc", 
+      "ActiveRecord::ConnectionAdapters::ODBCAdapter", 
+      "active_record/connection_adapters/odbc_adapter.rb"
+    )
+  end
+end
